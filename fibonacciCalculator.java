eb4d6b743a24faf3533286e8d7a6bbebe55032ef
SabraMGrace <sabramgrace@gmail.com>
2019-10-07 16:40:19 -0700

    Update fibonacciCalculator.java

--- fibonacciCalculator.java
+++ fibonacciCalculator.java
@@ -10,11 +10,13 @@ package fibonacciCalculator;
 public class fibonacciCalculator {
 
 	public static void main(String[] args) {
-		// Use a for loop to compute and print out the first 12 Fibonacci numbers.
+		// Use a for loop to compute and print out the first 12 Fibonacci numbers, beginning with 1 (using the 5th edition textbook).
+		// Main function fibCalc
 		System.out.println("This program calculates the first 12 Fibonacci numbers beginning with 1 (using the 5th textbook edition).");
 		
 		fibCalc();
 	}
+		// Use a for loop to compute and print out the first 12 Fibonacci numbers.
 	public static void fibCalc(){
 		int previous = 0; 
 		int current = 1;
