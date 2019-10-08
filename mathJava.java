370c70086fc95ca3c592f88e6b8937e60d7eb769
SabraMGrace <sabramgrace@gmail.com>
2019-10-07 18:45:33 -0700

    Create mathJava.java

---
+++ mathJava.java
@@ -0,0 +1,16 @@
+package mathPractice;
+
+public class Math {
+
+	public static void main(String[] args) {
+		
+		double squareRoot = Math.sqrt(121.0);
+		System.out.println(squareRoot);
+		
+		double abs = Math.abs(-50.0);
+		System.out.println(abs);
+		
+		double pow = Math.pow(2.0, 5.0);
+		System.out.println(pow);
+	}
+}
