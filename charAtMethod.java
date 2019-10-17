21f9cbf42bed8fafbcf3292f385f8e934618da4d
SabraMGrace <sabramgrace@gmail.com>
2019-10-16 18:53:55 -0700

    Create charAtMethod.java

---
+++ charAtMethod.java
@@ -0,0 +1,15 @@
+package oct16Test1;
+
+public class CharPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String major = "CSE";
+System.out.println(major.length());
+for (int i = 0; i < major.length(); i++) {
+	char c = major.charAt(i);
+	System.out.println(c);
+}
+	}
+
+}
