b23868224664328815a05b326bbceef4dcfa0dfb
SabraMGrace <sabramgrace@gmail.com>
2019-10-21 18:25:06 -0700

    Rename printNumbers.java to printNumbersOct21.java

---
+++ printNumbersOct21.java
@@ -0,0 +1,17 @@
+package oct21;
+
+public class Oct21 {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		printNumbers(5);
+	}
+		public static void printNumbers(int max) {
+			for (int i = 1; i <= max; i++) {
+				System.out.print(" " + i);
+			}
+		System.out.println();
+	}
+
+}
