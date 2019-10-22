26e9020c8987f4ec4ddc0ebd7c2547adf72ae348
SabraMGrace <sabramgrace@gmail.com>
2019-10-21 18:26:32 -0700

    Create oct21WhileLoopPractice.java

---
+++ oct21WhileLoopPractice.java
@@ -0,0 +1,17 @@
+package oct21;
+
+public class Oct21 {
+
+	public static void main(String[] args) {
+		
+		int num = 1;                      // initialization
+		while (num <= 200) {              // test
+		    System.out.print(num + " ");
+		    num = num * 2;                // update
+			}
+		System.out.println();
+	}
+
+}
+
+	// output:  1 2 4 8 16 32 64 128
