30172dcf449d3d78099a563f7168bdd0807bfe2a
SabraMGrace <sabramgrace@gmail.com>
2019-11-12 20:19:15 -0800

    Update boyGirl.java

--- boyGirl.java
+++ boyGirl.java
@@ -1,7 +1,9 @@
 //*******************************************************************
 // ITC 115 (Fall 2019)
 // boyGirl
-// Pull file for boyGirl method
+// Read input from file for boyGirl method.
+// Print how many boys and girls. 
+// Print the difference between the boys and girls' added ages.
 // By: Sabra Grace
 // Date: 11/9/19
 //*******************************************************************
@@ -11,23 +13,22 @@ package boyGirl;
 import java.io.*;
 import java.util.*;
 
+// create Scanner to pull file data.
 public class BoyGirl {
-
-	public static void main(String[] args) {
-		boygirl();
+	public static void main(String[] args) 
+			throws FileNotFoundException {
+		 Scanner input = new Scanner(new File("boysgirls.txt"));
+		 boyGirl(input);
+}
+// define boyGirl method.
+	public static void boyGirl(Scanner input) {
+		while (input.hasNext()) {
+			String name = input.next();
+	        double sum = 0.0;
+	        while (input.hasNextDouble()) {
+	            sum += input.nextDouble();
+	        }
+	        System.out.println("Sum = " + sum + name);
+	}
 	}
 }
-	public static void boygirl() {
-		throws FileNotFoundException {
-	        input = new Scanner(new File("boysgirls.txt"));
-	        double sum = 0.0;
-	        int count = 0;
-	        while (input.hasNextDouble()) {
-	            double next = input.nextDouble();
-	            count++;
-	            System.out.println("number " + count + " = " + next);
-	            sum += next;
-	        }
-	        System.out.println("Sum = " + sum);
-
-	}
