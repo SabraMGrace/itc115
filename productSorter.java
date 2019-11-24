e229c6e264949e5d4bf701dce5f30c26db7909cd
SabraMGrace <sabramgrace@gmail.com>
2019-11-24 14:58:50 -0800

    Update productSorter.java

--- productSorter.java
+++ productSorter.java
@@ -1,1 +1,15 @@
+//*******************************************************************
+// ITC 115 (Fall 2019)
+// productSorter
+// 1. The product data should include a product code, description, price and product count. 
+// 2. In addition to the getter and setter methods, the class should implement toString and also a formatted output of the price (Hint: use the NumberFormat class. Here is the implementation of your formattedPrice method: 
+// Ex: public String getPriceFormatted() 
+//    {
+//        String formattedPrice = NumberFormat.getCurrencyInstance().format(this.price);
+//        return formattedPrice;
+//    }
+// 3. From the client app create instance of 2-3 different products and use the toString method to display their data.
+// Optional: Keep track of the count of the products created and call the getCount method to verify that it is correct. (Where could you keep track of the count of products?)
+// By: Sabra Grace
+//*******************************************************************
 
